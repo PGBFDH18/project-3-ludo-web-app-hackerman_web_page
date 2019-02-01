6534d480f30ceb06a06b3eba49f73ea7c0c84100
olssonolof <44023615+olssonolof@users.noreply.github.com>
2019-02-01 20:12:16 +0100

    Added a post method to the api

--- src/Hackerman_WebbApp/Controllers/LudoController.cs
+++ src/Hackerman_WebbApp/Controllers/LudoController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 
 namespace Hackerman_WebbApp.Controllers
 {
+
+    [Route("Ludo")]
     public class LudoController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewGame()
+        {
+            var client = new RestClient("http://localhost:57659/");
+
+            var request = new RestRequest("/newgame", Method.POST);
+            
+            IRestResponse<int> createGameResponse = client.Execute<int>(request);
+
+            var output = createGameResponse.Content;
+
+            return View();
+
+
+
         }
     }
 }
---
+++ src/TestOfApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Hackerman_WebbApp.Controllers;
+
+
+namespace TestOfApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LudoController.NewGame();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
